ed81d884e71b6e091b75e410ea118c4053b6910a
Saadia B <saadiabouloudene@gmail.com>
2025-06-08 10:48:03 +0200

    feat: add dockerignore and replace cleaning_dat with cleaner.py

---
+++ cleaner.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import re
+from datetime import datetime
+
+def clean_emojis(text):
+    # Suppression simple d'emojis (exemple générique, à adapter si tu veux plus précis)
+    emoji_pattern = re.compile(
+        "["
+        "\U0001F600-\U0001F64F"  # emoticônes
+        "\U0001F300-\U0001F5FF"  # symboles et pictogrammes
+        "\U0001F680-\U0001F6FF"  # transports et cartes
+        "\U0001F1E0-\U0001F1FF"  # drapeaux (iOS)
+        "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', text)
+
+def clean_text(text):
+    if pd.isna(text):
+        return ""
+    # Supprime emojis
+    text = clean_emojis(text)
+    # Supprime les espaces multiples et sauts de ligne
+    text = re.sub(r"\s+", " ", text)
+    return text.strip()
+
+def clean_csv(input_file, output_file):
+    # Charger le CSV
+    df = pd.read_csv(input_file, dtype=str)
+    
+    # Vérification du nombre de colonnes
+    expected_cols = ['review_id', 'rating', 'content', 'author', 'publication_date', 'scrape_date']
+    if list(df.columns) != expected_cols:
+        raise ValueError(f"Le fichier CSV a une structure invalide : {list(df.columns)}. Attendu : {expected_cols}")
+    # Nettoyer les commentaires
+    df['content'] = df['content'].astype(str).apply(clean_text)
+    
+    # Supprimer les lignes où content est vide ou juste un "?"
+    df = df[~df['content'].isin(["?", ""])]
+    
+    # supprimer les lignes où content est NaN ou vide
+    df = df[~df['content'].isna()]
+    df = df[df['content'].str.strip() != ""]
+
+    #supprimer les lignes en double pour les champs 'content' et 'author' similaire
+    df = df.drop_duplicates(subset=['content', 'author'], keep='first')
+
+    # Sauvegarder le CSV nettoyé
+    df.to_csv(output_file, index=False, encoding='utf-8')
+    print(f"Fichier nettoyé sauvegardé dans {output_file}")
+
+# if __name__ == "__main__":
+#     input_file = 'data/avis_boutique.csv'
+#     output_file = 'data/avis_nettoyes.csv'
+#     clean_csv(input_file, output_file)
